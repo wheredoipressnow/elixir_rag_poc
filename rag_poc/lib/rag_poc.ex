c6d6fc23905f5ad8c6278feb70c06120dfc08758
Lars Oehnenamen <wheredoipressnow@users.noreply.github.com>
2026-04-04 08:21:01 +0300

    Scaffold Elixir project

---
+++ rag_poc/lib/rag_poc.ex
@@ -0,0 +1,18 @@
+defmodule RagPoc do
+  @moduledoc """
+  Documentation for `RagPoc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RagPoc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
